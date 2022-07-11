13ec4f1f84437560bafecaec9026cfc8a4168528
Aleksander de Rosset <aleksander.de.rosset@gmail.com>
2022-07-11 14:37:02 +0200

    test

--- src/utils.py
+++ src/utils.py
@@ -58,7 +58,7 @@ def get_geo_location():
         geo_lat = translate(geo_lat_raw, COORDINATES_TRANSLATE)
         geo_long_raw = re.search(pattern=LONGITUDE_PATTERN, string=city_raw, flags=re.MULTILINE).group().rstrip()
         geo_long = translate(geo_long_raw, COORDINATES_TRANSLATE)
-        locations[city] = [geo_lat, geo_long]
+        locations[city] = {"lat":geo_lat,"lon": geo_long}
     return locations
 
 
